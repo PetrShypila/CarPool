0452ad33659ffaa93fa2e2ea1dd363f09f0623cc
Petr Shypila <petr.shypila@lekta.ai>
2017-10-31 15:04:55 +0100

    Hello world webpage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-/* eslint-disable no-console */
-console.log('Hello world!');
+import './styles/styles.css';
+
+import 'babel-polyfill';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+
+import App from './components/App';
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render((<App/>), document.getElementById('app'));
