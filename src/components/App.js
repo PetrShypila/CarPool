1d5a8ad941f2ef7f3337ca3b5353b7441c1f2666
Petr Shypila <petr.shypila@lekta.ai>
2017-11-16 22:20:19 +0100

    Added GMaps API key

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const App = () => (
   <Switch>
     <Route exact path="/" component={Index} />
     <Route path="/map" render={(props) => <Map isMarkerShown
-                                           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                                           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB8TVrna1xySPsYSqwlK_Ek0rG25uzYWI0"
                                            loadingElement={<div style={{ height: `100%` }} />}
                                            containerElement={<div style={{ height: `800px` }} />}
                                            mapElement={<div style={{ height: `100%` }} />}
