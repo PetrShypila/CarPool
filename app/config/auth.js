9a5ef317cf15365ce35da0bd0a69d6e02aee0146
Petr Shypila <iKrumping@gmail.com>
2018-02-10 18:08:07 +0100

    Updated deploy configs

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,6 +1,6 @@
 export default {
   key: "X-Api-Key",
   val: "3yr3472ytv734vt894ucy3y8q43t7347vwt",
-  loginUrl:"http://localhost:4000/login",
-  signupUrl:"http://localhost:4000/signup"
+  loginUrl:"https://cpoolrauth.herokuapp.com/login",
+  signupUrl:"https://cpoolrauth.herokuapp.com/signup"
 };
